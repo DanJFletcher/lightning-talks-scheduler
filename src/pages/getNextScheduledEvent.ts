2bff9a12d385576c3bbb4c15f4b44f1d8b592de6
Dan Fletcher <dannyjfletcher@outlook.com>
2021-05-06 09:17:58 -0400

    Refactor for readability

--- src/pages/getNextScheduledEvent.ts
+++ src/pages/getNextScheduledEvent.ts
@@ -10,20 +10,25 @@ const isToday = (date: Date) => {
   // )
 }
 
-export const getNextScheduledEvent = (dates: ScheduledEvent[]) => {
+export const getNextScheduledEvent = (scheduledEvents: ScheduledEvent[]) => {
   const now = new Date()
   const MAX_DATE = 8640000000000000
-  let nextEvent: ScheduledEvent = { date: new Date(MAX_DATE).toString(), id: 0 }
+  const anEventWayOutInTheFuture: ScheduledEvent = {
+    date: new Date(MAX_DATE).toString(),
+    id: 0,
+  }
 
-  dates.forEach((date) => {
-    const currentDate = new Date(date.date)
+  let nextEvent = anEventWayOutInTheFuture
+
+  scheduledEvents.forEach((event) => {
+    const currentDate = new Date(event.date)
     console.log(currentDate, currentDate >= now)
 
     if (
       (currentDate >= now || isToday(currentDate)) &&
       currentDate < new Date(nextEvent.date)
     ) {
-      nextEvent = date
+      nextEvent = event
     }
   })
 
