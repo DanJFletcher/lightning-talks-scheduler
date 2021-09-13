098affba9abe7f624178263709af6cade99705ac
Dan Fletcher <dannyjfletcher@outlook.com>
2021-09-13 16:48:52 -0400

    Add deps

--- .scripts/testServer.js
+++ .scripts/testServer.js
@@ -1,12 +1,9 @@
 import path from 'path'
 import express from 'express'
-import history from 'connect-history-api-fallback'
-import { frontendPort } from '../src/utils/portUtils'
 require('dotenv').config()
 
 const app = express()
 
-app.use(history())
 app.use(express.static(path.join(__dirname, '../build')))
 
 app.listen(process.env.REACT_TESTING_PORT | 3000)
